91959cafb5b0b042e98fd948652a9bdfcd212dfd
nobbavet <70631463+nobbavet@users.noreply.github.com>
2021-06-14 09:08:08 +0200

    no message

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -5,12 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/kleurtjesv2.css">
-    <title>Document</title>
+    <title>DOCUMENT2</title>
 </head>
 <body>
-    <header><h1>hoi haroub</h1></header>
-    <main> </main>
-    <aside> </aside>
-    <footer> </footer>
+    <div class="wrapper" id="wrapper">
+        <header>
+            <h1>PORTO</h1>
+        </header>
+        <main> 
+
+        </main>
+        <aside> 
+
+        </aside>
+        <footer> 
+            
+        </footer>
+    </div>    
 </body>
 </html>
