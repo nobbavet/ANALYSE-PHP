9ec45c0ee9a78c36de04a845e42a623c1969b970
nobbavet <70631463+nobbavet@users.noreply.github.com>
2021-06-24 13:46:45 +0200

    no message

---
+++ example-app/app/Http/Controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/* Heb een main controller aangemaakt om naar andere pagina's toe te gaan */
+
+class MainController extends Controller
+{
+    public function hobby(Request $request)
+    {
+        // dd($request);
+        return view('hobby');
+    }
+    // public function
+}
