2bfb311e88dcefaf5200ec5b810824b2b2ab0a6b
nobbavet <70631463+nobbavet@users.noreply.github.com>
2021-06-26 10:25:38 +0200

    no message

--- example-app/resources/views/hobby.blade.php
+++ example-app/resources/views/hobby.blade.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/kleurtjesv2.css">
     <title>Document</title>
 </head>
 <body>
-    <header><h1>HOBBY'S</h1></header>
+    <div class="wrapper" id="wrapper">
+        <header>        
+            <h1>HOBBY'S</h1>
+            <nav> 
+                <!-- /* navbar toegevoegd voordat ik een hamburger menu ging maken */ -->
+                <ul> 
+                    <li><a href="/">Home</a></li> 
+                    <li><a class="active" href="hobby">Hobby's</a></li>                
+                </ul>
+            </nav>
+        </header>
+        <main> 
+            <h2></h2>
+        </main>
+        <footer>
+            <p>Dipshit&Co</p> 
+        </footer>
+    </div>    
 </body>
 </html>
--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/kleurtjesv2.css">
+
     <title>DOCUMENT2</title>
 </head>
 <body>
@@ -12,14 +13,20 @@
     <div class="wrapper" id="wrapper">
         <header>        
             <h1>PORTO</h1>
-            <button> </button>
+            <nav>
+                <ul>
+                    <li><a class="active" href="#home">Home</a></li>
+                    <li><a href="hobby">Hobby's</a></li>                
+                </ul>
+            </nav>
+            <!-- <img src="hamboerger.png" alt=""> -->
         </header>
         <main> 
             <h2>ELLOOOOW</h2>
         </main>
-        <aside> 
+        <!-- <aside> 
             <h2>Menu</h2>
-        </aside>
+        </aside> -->
         <footer>
             <p>Dipshit&Co</p> 
         </footer>
