0d77266c39b1728c75f74713a24477dfa90a5f6f
nobbavet <70631463+nobbavet@users.noreply.github.com>
2021-06-30 08:40:11 +0200

    no message

--- example-app/resources/views/WieBenIk.blade.php
+++ example-app/resources/views/WieBenIk.blade.php
@@ -10,4 +10,17 @@
                 </ul>
             </nav>
     </header>
+    <div class="side">
+        <div class="info">
+            <h1>
+                Dit ben ik:
+                <img src="" alt="Nabil Ahammar">
+            </h1>
+            <article>
+                <h2>Naam: Nabil Ahammar</h2>
+                <h2>Geboorte datum: 15 oktober 2003</h2>
+
+            </article>
+        </div>
+    </div>
 @endsection
