2d822e31e2631fbad318beb6c663b894ca703d06
nobbavet <70631463+nobbavet@users.noreply.github.com>
2021-07-20 15:25:47 +0200

    beetje extra info er in gedaan

--- example-app/resources/views/WieBenIk.blade.php
+++ example-app/resources/views/WieBenIk.blade.php
@@ -17,10 +17,11 @@
             </h1>
                 <img src="{{asset('images/nobba.png')}}" alt="itz a me nabilo">
             <article>
-                <h2>Naam: Nabil Ahammar</h2>
-                <h2>Geboorte datum: 15 oktober 2003</h2>
-                <h2>Woonplaats: Friesland, Wolvega</h2>
-                <h2>Adres: Sprietstraat 4, 8471 GR</h2>
+                <h2>➼ Naam: Nabil Ahammar</h2>
+                <h2>➼ Nationaliteit: Nederlands</h2>
+                <h2>➼ Geboorte datum/plaats: 15 oktober 2003</h2>
+                <h2>➼ Woonplaats: Friesland, Wolvega</h2>
+                <h2>➼ Adres: Sprietstraat 4, 8471 GR</h2>
             </article>
         </div>
     </div>
@@ -29,8 +30,33 @@
             <h1>
             Opleidingen
             </h1>
-            <article>
-            
+            <article class="opleiding">
+            <p>➼ Theoretische leerweg op het Linde College 2015-2020.</p>
+            <p>➼ Software Development 2020-heden.</p>
+            </article>
+        </div>
+    </div>
+    <div class="m2">
+        <div class="art1">
+            <h1>
+            Stages
+            </h1>
+            <article class="stages">
+            <p>➼ Vakkenvuller Karwei, Wolvega 2017-2017.</p>
+            <p>➼ Doomijn KinderOpvang, Wolvega 2018-2018.</p>
+            </article>
+        </div>
+    </div>
+    <div class="m3">
+        <div class="art1">
+            <h1>
+            Talen kennis
+            </h1>
+            <article class="TALEN">
+            <p>➼ Nederlands, Moedertaal.</p>
+            <p>➼ Duits, Spreken & Verstaan.</p>
+            <p>➼ Engels, Spreken & Verstaan.</p>
+            <p>➼ Berbers (noordelijk Marokko), Verstaan.</p>
             </article>
         </div>
     </div>
